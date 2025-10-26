cfe16f2eb8db22cbefae28ed117a261b0fab91ed
Sneha Srivastava <srivastavasneha895@gmail.com>
2025-10-26 14:48:37 +0000

    Added

---
+++ Basics/forloop.py
@@ -0,0 +1,3 @@
+#using a for loop to print message multiple times
+for i in range(5):
+    print(f"Python is fun! {i+1}")
---
+++ Basics/variables.py
@@ -0,0 +1,4 @@
+# Basic variables and fstring in python
+name = "Enry"
+age = 20
+print(f"My name is {name} and I am {age} years old.")

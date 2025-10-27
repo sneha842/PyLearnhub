1c1d83f9e1b2243bd07e591934484b97b61f36ad
Sneha Srivastava <srivastavasneha895@gmail.com>
2025-10-27 11:00:02 +0000

    Added a datatypes file

---
+++ Basics/datatypes.py
@@ -0,0 +1,33 @@
+#Python Data Types
+
+# Integer
+num_students = 25
+print("Number of students:", num_students, "| Type:", type(num_students))
+
+# Float
+average_score = 82.6
+print("Average score:", average_score, "| Type:", type(average_score))
+
+# String
+course_name = "Python for Beginners"
+print("Course name:", course_name, "| Type:", type(course_name))
+
+# Boolean
+is_enrolled = True
+print("Enrollment status:", is_enrolled, "| Type:", type(is_enrolled))
+
+# List
+topics = ["Variables", "Loops", "Functions", "OOP"]
+print("List of topics:", topics, "| Type:", type(topics))
+
+# Tuple (immutable list)
+coordinates = (10, 20)
+print("Coordinates:", coordinates, "| Type:", type(coordinates))
+
+# Dictionary
+student = {"name": "Enry", "age": 20, "course": "Btech"}
+print("Student info:", student, "| Type:", type(student))
+
+# Set (unique unordered elements)
+unique_numbers = {1, 2, 3, 3, 2, 1}
+print("Unique numbers:", unique_numbers, "| Type:", type(unique_numbers))
